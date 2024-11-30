d3a6ad0f7971f2d6c7261ad2b6ee7366cf75cd5c
Biswaranjan <74249139+Biswaranjan-cyber@users.noreply.github.com>
2024-12-01 00:18:57 +0530

    Update Line_follower.c

--- Line_follower.c
+++ Line_follower.c
@@ -1,17 +1,19 @@
-
+// Sensor pins
 const int leftSensorPin = 2;  
-const int rightSensorPin = 3; 
+const int rightSensorPin = 3;  
 
-
+// Motor control pins
 const int leftMotorPin1 = 4; 
 const int leftMotorPin2 = 5;
 const int rightMotorPin1 = 6;
 const int rightMotorPin2 = 7;
 
 void setup() {
-
+  // Configure sensor pins as inputs
   pinMode(leftSensorPin, INPUT);
   pinMode(rightSensorPin, INPUT);
+
+  // Configure motor control pins as outputs
   pinMode(leftMotorPin1, OUTPUT);
   pinMode(leftMotorPin2, OUTPUT);
   pinMode(rightMotorPin1, OUTPUT);
@@ -19,27 +21,27 @@ void setup() {
 }
 
 void loop() {
-
+  // Read sensor values
   int leftSensorValue = digitalRead(leftSensorPin);
   int rightSensorValue = digitalRead(rightSensorPin);
 
-
+  // Logic for robot movement
   if (leftSensorValue == HIGH && rightSensorValue == HIGH) {
+    // Both sensors on the line
     moveForward();
-	
   } else if (leftSensorValue == LOW && rightSensorValue == HIGH) {
+    // Left sensor off the line, turn right
     turnRight();
-	
   } else if (leftSensorValue == HIGH && rightSensorValue == LOW) {
+    // Right sensor off the line, turn left
     turnLeft();
-	
   } else {
-    stop();
-	
+    // Both sensors off the line, stop
+    stopMotors();
   }
 }
 
-
+// Function to move the robot forward
 void moveForward() {
   digitalWrite(leftMotorPin1, HIGH);
   digitalWrite(leftMotorPin2, LOW);
@@ -47,7 +49,7 @@ void moveForward() {
   digitalWrite(rightMotorPin2, LOW);
 }
 
-
+// Function to turn the robot left
 void turnLeft() {
   digitalWrite(leftMotorPin1, LOW);
   digitalWrite(leftMotorPin2, LOW);
@@ -55,6 +57,7 @@ void turnLeft() {
   digitalWrite(rightMotorPin2, LOW);
 }
 
+// Function to turn the robot right
 void turnRight() {
   digitalWrite(leftMotorPin1, HIGH);
   digitalWrite(leftMotorPin2, LOW);
@@ -62,7 +65,8 @@ void turnRight() {
   digitalWrite(rightMotorPin2, LOW);
 }
 
-void stop() {
+// Function to stop the motors
+void stopMotors() {
   digitalWrite(leftMotorPin1, LOW);
   digitalWrite(leftMotorPin2, LOW);
   digitalWrite(rightMotorPin1, LOW);
